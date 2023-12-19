277f6197fc448ee9966ce5513dd219746ecb6a59
sarathrajan922 <sarathrajan922@gmail.com>
2023-12-19 17:53:43 +0530

    userPersonalInfoUpload API done in clientside

---
+++ client/features/axios/api/user/userPersonalInfo.ts
@@ -0,0 +1,26 @@
+import axios,{AxiosRequestConfig} from "axios";
+import BASE_URL,{urls} from "@/config";
+import userSetupAxiosInterceptors from "../../interceptors/userInterceptor";
+
+
+const api = userSetupAxiosInterceptors();
+
+const userPersonalInfoUpload = async(values:any)=>{
+    try{
+        const config:AxiosRequestConfig = {
+            url:BASE_URL+urls.USER_PERSONAL_INFO_SUBMIT,
+            method:"post",
+            data:values
+        }
+        const response = await api(config)
+        return response?.data
+    }catch(error: any){
+        if(error.message === 'Request failed with status code 404'){
+         throw new Error('user not exists !!!');
+        }else{
+          throw new Error('Signup failed ,try again')
+        }
+      }
+}
+
+export default userPersonalInfoUpload;
