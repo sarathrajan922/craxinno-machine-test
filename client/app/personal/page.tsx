ea63a629571a080ce467db7fd0ed291e6085063f
sarathrajan922 <sarathrajan922@gmail.com>
2023-12-21 03:07:05 +0530

    redux store created

---
+++ client/app/personal/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PersonalInfo from "@/components/personalInfo";
+import Header from "@/components/header";
+
+const PersonalInfoForm = () => {
+  return (
+    <>
+      <Header />
+      <main className="flex min-h-screen flex-col items-center justify-between  bg-white">
+        <PersonalInfo />
+      </main>
+    </>
+  );
+};
+
+export default PersonalInfoForm;
---
+++ client/features/redux/app/Store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { userReducer } from '../slices/userSlice'
+
+
+export const store = configureStore({
+    reducer:{
+        userData :userReducer
+    }
+})
+
+export type State = typeof store
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export default store
